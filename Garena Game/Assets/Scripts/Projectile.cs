401056e292c109aff1ae7cce2ca0f131a180bca2
alifsam74 <chiyodewa74@gmail.com>
2023-12-09 12:37:02 +0700

    Enemy Projectile Fix & Enemy Melee Range Balancing

--- Garena Game/Assets/Scripts/Projectile.cs
+++ Garena Game/Assets/Scripts/Projectile.cs
@@ -6,7 +6,6 @@ public class Projectile : MonoBehaviour
 {
     public float Force;
     Rigidbody2D rb;
-    Vector2 Direction;
 
     static Object[] spriteArray;
 
@@ -24,23 +23,16 @@ public class Projectile : MonoBehaviour
     void Start()
     {
         Transform player = FindAnyObjectByType<PlayerMovementController>().transform;
-        Direction = player.position - transform.position;
+        Vector3 Direction = player.position - transform.position;
         Vector3 rotation = transform.position - player.position;
+        rb.velocity = new Vector2(Direction.x, Direction.y).normalized * Force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
 
         int chosenSprite = Random.Range(0, spriteArray.Length);
         this.GetComponent<SpriteRenderer>().sprite = Instantiate(spriteArray[chosenSprite]) as Sprite;
-
-
-        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(Direction.x, Direction.y).normalized * Force;
-        transform.Rotate(new Vector3(0, 0, 3));
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
